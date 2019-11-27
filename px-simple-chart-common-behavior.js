7478e9a47690e0dec34356883689af2040028dea
Justin Kirkwood <justin@corvuscreative.ca>
2019-11-27 12:53:44 -0800

    Upgrade from Polymer 2 to 3 via Polymer CLI Modulizer
    Utilized the Polymer CLI Modulizer to convert to javascript imports for Polymer 3

--- px-simple-chart-common-behavior.js
+++ px-simple-chart-common-behavior.js
@@ -1,4 +1,4 @@
-<!--
+/*
 Copyright (c) 2018, General Electric
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,9 +12,12 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
--->
-
-<script>
+*/
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
 var PxSimpleChartCommonBehavior = PxSimpleChartCommonBehavior || {};
 
 /*
@@ -84,7 +87,7 @@ PxSimpleChartCommonBehavior.common = {
       width: {
         type: String,
         observer: '_drawChart',
-        value: this._defaultWidth
+        value: window._defaultWidth
       },
 
       /**
@@ -93,7 +96,7 @@ PxSimpleChartCommonBehavior.common = {
       height: {
         type: String,
         observer: '_drawChart',
-        value: this._defaultHeight
+        value: window._defaultHeight
       }
 
     },
@@ -382,4 +385,3 @@ PxSimpleChartCommonBehavior.common = {
 
 // Maintain backward compatability to call behavior with name `pxSimpleChartCommonBehavior`
 window.pxSimpleChartCommonBehavior = PxSimpleChartCommonBehavior.common;
-</script>
